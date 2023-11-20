117f5a30844a5acb1e9f009d224003a8dca1f541
DerIgnotus <jawoll1909@gmail.com>
2023-11-20 22:12:39 +0100

    Icon PNG in Assets folder + cargo fmt + cargo clippy
    I just cleaned it up a bit and tried cargo fmt and cargo clippy

--- clicker_game/src/main.rs
+++ clicker_game/src/main.rs
@@ -1,24 +1,22 @@
+use bevy::winit::WinitWindows;
 use bevy::{
-    prelude::*,
-    window::{PresentMode, WindowTheme, PrimaryWindow},
     input::common_conditions::input_toggle_active,
+    prelude::*,
+    window::{PresentMode, PrimaryWindow, WindowTheme},
 };
 use bevy_egui::{EguiContext, EguiPlugin};
 use bevy_inspector_egui::{
     bevy_inspector::hierarchy::SelectedEntities, DefaultInspectorConfigPlugin,
 };
-use bevy::winit::WinitWindows;
 use std::io::Cursor;
 use winit::window::Icon;
-
-
 
 #[derive(Component)]
 struct MainCamera;
 
 #[derive(Resource, Default)]
 struct MyWorldCoords {
-    x: f32, 
+    x: f32,
     y: f32,
 }
 
@@ -32,43 +30,32 @@ struct ToggleBool {
     toggle_print: bool,
 }
 
-
 fn main() {
-    App::new() 
-        .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "Clicker Game Version 1.0!".into(),
-                    position: WindowPosition::At(IVec2 { x: (360), y: (140) }), //Here you can change it 
-                    resolution: (1200.0, 800.0).into(), // You can also do WindowPosition::Centered(Current) or
-                    present_mode: PresentMode::AutoVsync, // WindowPosition::Automatic
-                    fit_canvas_to_parent: true,
-                    prevent_default_event_handling: false,
-                    window_theme: Some(WindowTheme::Dark),
-                    ..default()
-                }),
+    App::new()
+        .add_plugins((DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Clicker Game Version 1.0!".into(),
+                position: WindowPosition::At(IVec2 { x: (360), y: (140) }), //Here you can change it
+                resolution: (1200.0, 800.0).into(), // You can also do WindowPosition::Centered(Current) or
+                present_mode: PresentMode::AutoVsync, // WindowPosition::Automatic
+                fit_canvas_to_parent: true,
+                prevent_default_event_handling: false,
+                window_theme: Some(WindowTheme::Dark),
                 ..default()
             }),
-        ))
+            ..default()
+        }),))
         .add_plugins(EguiPlugin)
         .add_plugins(DefaultInspectorConfigPlugin)
-        .add_systems(PostStartup, 
-            (
-            post_startup,
-            )
-        )
-        .add_systems(Startup, 
-            (
-                setup,
-                set_window_icon,
-            ) 
-        )
-        .add_systems(Update, 
+        .add_systems(PostStartup, (post_startup,))
+        .add_systems(Startup, (setup, set_window_icon))
+        .add_systems(
+            Update,
             (
                 mouse_button_input,
                 my_cursor_system,
                 inspector_ui.run_if(input_toggle_active(true, KeyCode::Escape)),
-            )
+            ),
         )
         .run();
 }
@@ -123,47 +110,36 @@ fn inspector_ui(world: &mut World, mut selected_entities: Local<SelectedEntities
 
                 ui.allocate_space(ui.available_size());
             });
-        }); 
+        });
 }
 
-
-fn setup (
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let texture = asset_server.load("Clicker.png");
-    commands.spawn ((
-        Camera2dBundle {
-            ..default()
-        },
-        MainCamera,
-    ));
+    commands.spawn((Camera2dBundle { ..default() }, MainCamera));
 
-    commands.spawn((
-        SpriteBundle {
-            texture,
-            ..default()
-        },
-    ));
+    commands.spawn((SpriteBundle {
+        texture,
+        ..default()
+    },));
 
     commands.init_resource::<MyWorldCoords>();
     commands.insert_resource::<Money>(Money { amount: 0 });
-    commands.insert_resource::<ToggleBool>(ToggleBool { toggle_print: false });
+    commands.insert_resource::<ToggleBool>(ToggleBool {
+        toggle_print: false,
+    });
 }
 
-fn post_startup () {
+fn post_startup() {
     println!("\n\n Game Starts, Have Fun!\n");
 }
 
-fn set_window_icon (
+fn set_window_icon(
     windows: NonSend<WinitWindows>,
     primary_window: Query<Entity, With<PrimaryWindow>>,
 ) {
     let primary_entity = primary_window.single();
     let primary = windows.get_window(primary_entity).unwrap();
-    let icon_buf = Cursor::new(include_bytes!(
-        "icon_256x256.png"
-    ));
+    let icon_buf = Cursor::new(include_bytes!("../Assets/icon_256x256.png"));
     if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
         let image = image.into_rgba8();
         let (width, height) = image.dimensions();
@@ -173,7 +149,7 @@ fn set_window_icon (
     };
 }
 
-fn mouse_button_input (
+fn mouse_button_input(
     buttons: Res<Input<MouseButton>>,
     mycoords: Res<MyWorldCoords>,
     toggle_bool: Res<ToggleBool>,
@@ -181,7 +157,7 @@ fn mouse_button_input (
 ) {
     if buttons.just_pressed(MouseButton::Left) {
         println!("MouseButtonWasPressed");
-        if cursor_in_clicker(&*mycoords, &*toggle_bool) {
+        if cursor_in_clicker(&mycoords, &toggle_bool) {
             money.amount += 10;
             println!("Money: {}", money.amount);
         }
@@ -192,11 +168,10 @@ fn cursor_in_clicker(mycoords: &MyWorldCoords, toggle_bool: &ToggleBool) -> bool
     if toggle_bool.toggle_print {
         eprintln!("Cursor X: {}\nCursor Y: {}", mycoords.x, mycoords.y);
     }
-    (mycoords.x <= 100.0 && mycoords.x >= -100.0) &&
-    (mycoords.y <= 100.0 && mycoords.y >= -100.0)
+    (mycoords.x <= 100.0 && mycoords.x >= -100.0) && (mycoords.y <= 100.0 && mycoords.y >= -100.0)
 }
 
-fn my_cursor_system (
+fn my_cursor_system(
     mut mycoords: ResMut<MyWorldCoords>,
     q_window: Query<&Window, With<PrimaryWindow>>,
     q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
@@ -204,7 +179,8 @@ fn my_cursor_system (
     let (camera, camera_transform) = q_camera.single();
     let window = q_window.single();
 
-    if let Some(world_position) = window.cursor_position()
+    if let Some(world_position) = window
+        .cursor_position()
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
         .map(|ray| ray.origin.truncate())
     {
---
+++ clicker_game/target/debug/deps/clicker_game-7af2731b4653da7e.d
@@ -0,0 +1,10 @@
+F:\Github\Clicker-Game\clicker_game\target\debug\deps\clicker_game-7af2731b4653da7e.rmeta: src\main.rs src\../Assets/icon_256x256.png Cargo.toml
+
+F:\Github\Clicker-Game\clicker_game\target\debug\deps\clicker_game-7af2731b4653da7e.d: src\main.rs src\../Assets/icon_256x256.png Cargo.toml
+
+src\main.rs:
+src\../Assets/icon_256x256.png:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
